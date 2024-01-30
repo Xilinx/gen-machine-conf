7a8175ead223b4cf9766eb687ca0d84aa5e23da4
Swagath Gadde <swagath.gadde@amd.com>
2024-01-29 23:50:44 -1200

    plnx_machine.py: add xen support for plnx in sdt flow.
    Add xen required variables for petalinux with decoupling flow.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>
    Acked-by: Varalaxmi Bingi <Varalaxmi.bingi@amd.com>

--- lib/plnx_machine.py
+++ lib/plnx_machine.py
@@ -540,7 +540,13 @@ def GeneratePlnxConfig(args, machine_conf_file):
         override_string += 'SYMLINK_FILES:%s = "%s:%s"\n' \
             % (soc_family, 'system-default.dtb', 'system.dtb')
         override_string += 'DEVICE_TREE_NAME = "system.dtb"\n'
-    override_string += 'BOOTMODE = "generic"\n'
+
+    if (hw_flow == 'sdt') and (dt_xenhw_dtsi or dt_xenqemu_dtsi):
+        override_string += 'BOOTMODE = "xen"\n'
+        override_string += 'ENABLE_XEN_UBOOT_SCR = "1"\n'
+    else:
+        override_string += 'BOOTMODE = "generic"\n'
+
     override_string += 'BOOTFILE_EXT = ""\n'
     # Use MACHINE as override due to u-boot-xlnx-scr has $soc_family-$soc_variant overrides
     override_string += 'RAMDISK_IMAGE:${MACHINE} = "rootfs.cpio.gz.u-boot"\n'
