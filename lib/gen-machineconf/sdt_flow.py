1f1fa95adf39a56f5e4049f37c891addcbb183ec
Chris Laplante <chris.laplante@agilent.com>
2025-01-09 12:09:00 -0500

    sdt_flow.py: remove an unused arg
    Signed-off-by: Chris Laplante <chris.laplante@agilent.com>

--- lib/gen-machineconf/sdt_flow.py
+++ lib/gen-machineconf/sdt_flow.py
@@ -713,7 +713,7 @@ def ParseSDT(args):
     if args.hw_flow == 'xsct':
         raise Exception('Invalide HW source Specified for System-Device-Tree.')
 
-    def gatherHWInfo(hw_file):
+    def gatherHWInfo():
         hw_info = {}
 
         logger.info('Getting Platform info from HW file')
@@ -795,7 +795,7 @@ def ParseSDT(args):
 
 
     #### Gather:
-    hw_info = gatherHWInfo(args.hw_file)
+    hw_info = gatherHWInfo()
 
     if hw_info['machine']:
         args.machine = hw_info['machine']
