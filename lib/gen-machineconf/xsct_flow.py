e93255c8b015c7aa0ad3e02fa8e7a0ee5fffcf2f
Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
2025-07-17 10:15:24 +0530

    gen-machine-conf: Update args with YAML data
    Retrive the args data from YAML and assign this as default values.
    Order of the args parsing is,
    	- default value assinged to arg
    	- args value from yaml
    	- commandline args value
    
    AddYamlDefaultValues function will read the data from yaml and parse
    each arg. This require arg name to read the data from yaml file for the
    particular arg.
    
    args:
      - --machine-name versal-vck190-xsct
      - --native-sysroot /proj/petalinux/2025.2/petalinux-v2025.2_daily_latest/tool/petalinux-v2025.2-final/sysroots/x86_64-petalinux-linux/
      - --hw-description https://petlainux.xilinx.com/2025.2/2025.2_0714_1_07150113/external/vck190-versal/vck190-versal-2025.2_0625_1.xsa
      - --require-machine vck190-versal --multiconfigfull
    
    Signed-off-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>
    
    Acked-for-series: Varalaxmi Bingi <Varalaxmi.bingi@amd.com>

--- lib/gen-machineconf/xsct_flow.py
+++ lib/gen-machineconf/xsct_flow.py
@@ -371,15 +371,19 @@ def register_commands(subparsers):
                                        ' <PATH_TO_XSA>/<xsa_name>.xsa] [other options]'
                                        )
     parser_xsa.add_argument('--xsct-tool', metavar='[XSCT_TOOL_PATH]',
+                            default=common_utils.AddYamlDefaultValues('--xsct-tool'),
                             help='Vivado or Vitis XSCT path to use xsct commands (Optional if you are already have AMD tools in your path)')
 
     parser_xsa.add_argument('-l', '--localconf', metavar='<config_file>',
+                            default=common_utils.AddYamlDefaultValues(['-l', '--localconf']),
                             help='Write local.conf changes to this file', type=os.path.realpath)
 
     parser_xsa.add_argument('--multiconfigfull', action='store_true',
+                            default=common_utils.AddYamlDefaultValues('--multiconfigfull'),
                             help='Generate/Enable Full set of multiconfig .conf and .dts files. Default is minimal')
 
     parser_xsa.add_argument('--multiconfigenable', action='store_true',
+                            default=common_utils.AddYamlDefaultValues('--multiconfigenable'),
                             help='Enable multiconfig support. default is disabled.')
 
     parser_xsa.set_defaults(func=ParseXsa)
